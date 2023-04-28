02ecba86f100cbc22516f60b639091bb9620e777
vinay <vinaychandola@gmail.com>
2023-04-29 01:07:55 +0530

    updating

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,19 +4,19 @@ import prismadb from "../../lib/prismadb";
 export default async function handler (req, res) {
   //creating users for the database;
 
-  await prismadb.user.create({
-    data:{
-      firstname:"hero",
-      lastname:"chandola",
-      username:"herovinay",
-      password:"herovinay"
-    }
-  })
-  res.status(200).json("ok");
+  // await prismadb.user.create({
+  //   data:{
+  //     firstname:"hero",
+  //     lastname:"chandola",
+  //     username:"herovinay",
+  //     password:"herovinay"
+  //   }
+  // })
+  // res.status(200).json("ok");
 
   //finding all users from the database;
-  // const alluser=await prismadb.user.findMany({});
-  // res.status(200).json(alluser);
+  const alluser=await prismadb.user.findMany({});
+  res.status(200).json(alluser);
 
 
   //updating data from the database and fetching updated data;
