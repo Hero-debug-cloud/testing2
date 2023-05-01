c36cba4a0432df08c9ea8f308844dbfe084cf205
vinay <vinaychandola@gmail.com>
2023-05-01 22:11:05 +0530

    updating

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,15 +4,46 @@ import prismadb from "../../lib/prismadb";
 export default async function handler (req, res) {
   //creating users for the database;
 
-  // await prismadb.user.create({
-  //   data:{
-  //     firstname:"hero",
-  //     lastname:"chandola",
-  //     username:"herovinay",
-  //     password:"herovinay"
-  //   }
-  // })
-  // res.status(200).json("ok");
+
+  if(req.method=="GET"){
+    res.status(200).json("Ok get request");
+  }
+
+  else if(req.method=="POST"){
+    const {firstname,lastname,username,password}=req.body;
+
+    const user=await prismadb.user.findMany({
+      where:{
+         OR:[
+          {
+            "firstname":firstname
+          },
+          {
+            "username":username
+          }
+         ]
+      }
+    })
+    console.log(user.length);
+    //if user is not present in the db;
+    if(!user.length){
+      await prismadb.user.create({
+        data:{
+          firstname,
+          lastname,
+          username,
+          password
+        }
+      })
+      res.status(200).json("Created User succesfully...");
+    }
+    else{
+      res.status(200).json("User Existed Already...");
+    }
+   
+  }
+
+  
 
   //finding all users from the database;
   // const alluser=await prismadb.user.findMany({});
@@ -21,18 +52,18 @@ export default async function handler (req, res) {
 
   //updating data from the database and fetching updated data;
   //where command is for only unique values in the db;
-  await prismadb.user.update({
-    where:{
-      firstname:"hero"
-    },
-    data:{
-      lastname:"chandole",
-      username:"vinni",
-      password:"vinni"
-    }
-  });
-  const updateduser=await prismadb.user.findMany({});
-  res.status(200).json(updateduser);
+  // await prismadb.user.update({
+  //   where:{
+  //     firstname:"hero"
+  //   },
+  //   data:{
+  //     lastname:"vibha",
+  //     username:"vibha",
+  //     password:"vibha"
+  //   }
+  // });
+  // const updateduser=await prismadb.user.findMany({});
+  // res.status(200).json(updateduser);
 
   // deleting user from the database;
   // await prismadb.user.delete({
